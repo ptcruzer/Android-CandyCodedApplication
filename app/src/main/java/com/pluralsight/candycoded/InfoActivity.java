8a6370db5f0b54e51585b55a38f46807ad523cc5
Mogammad <vx1@webmail.co.za>
2019-10-23 23:47:09 +0200

    Start the google maps activity Make createPhoneIntent(View view)

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,5 +45,6 @@ public class InfoActivity extends AppCompatActivity {
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
         phoneIntent.setData(Uri.parse("0123456789"));
         startActivity(phoneIntent);
+
     }
 }
